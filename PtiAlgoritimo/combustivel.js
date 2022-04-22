64c2d67999fda0d49e358590e485a612c3e7a6d3
Fabio S. Silva <fabio_silva76@outlook.com>
2022-04-22 15:08:55 -0300

    Update combustivel.js

--- PtiAlgoritimo/combustivel.js
+++ PtiAlgoritimo/combustivel.js
@@ -1,20 +1,36 @@
 
-var alc = parseInt(prompt('Digite o valor do Alcool'));
-var gas = parseInt(prompt('Digite o valor da gasolina'));
-var calc = alc / gas
-var res = calc
-
-
-    alert(res)
-if(res > 0.7){
+do{ 
+    var alc = parseInt(prompt('Digite o valor do Alcool'));
+    var gas = parseInt(prompt('Digite o valor da gasolina'));
+    var calc = alc / gas;
+    var res = calc;
+            
+    alert(res);
+    
+    
+    if(res > 0.7){
     
     alert('A melhor opção é colocar Gasolina ');
-} else if(res < 0.7){
+    } else if(res < 0.7){
 
     alert('A melhor opção é colocar Alcool ');
-} else if(res == 0.7){
+    } else if(res == 0.7){
 
     alert('Tanto faz colocar Alcool ou Gasolina');
-}
- var sim = prompt('Deseja efetuar uma nova consulta? \n Responda S para SIM \n ou \nN para Não!');
- var resp = sim
+
+    }
+    
+    var opc = prompt('Deseja efetuar uma nova consulta? \n Digite 1 para SIM \n ou  2  para Não!');
+    
+ } while( opc==1);{
+
+    alert('Obrigado!');
+ }
+
+   
+
+        
+
+  
+    
+
