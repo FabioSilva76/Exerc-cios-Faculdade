339dd4888345beea0e746087abee9bbb8f8d9744
Fabio S. Silva <fabio_silva76@outlook.com>
2022-04-21 23:10:12 -0300

    Trabalho PTI Faculdade

---
+++ PtiAlgoritimo/combustivel.js
@@ -0,0 +1,22 @@
+
+var alc = parseInt(prompt('Digite o valor do Alcool'));
+var gas = parseInt(prompt('Digite o valor da gasolina'));
+var calc = alc / gas
+var res = calc
+
+alert(res)
+
+if(res > 0.7){
+    
+    alert('A melhor opção é colocar Gasolina ');
+} else if(res < 0.7){
+
+    alert('A melhor opção é colocar Alcool ');
+} else if(res == 0.7){
+
+    alert('Tanto faz colocar Alcool ou Gasolina');
+}
+
+ var sim = prompt('Deseja efetuar uma nova consulta? \n Responda S para SIM ou N para Não!');
+ var resp = sim
+
