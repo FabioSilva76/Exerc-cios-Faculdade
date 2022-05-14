52d3e02e2e7716f435196733646321e7f0d32bb0
Fabio S. Silva <fabio_silva76@outlook.com>
2022-05-14 20:47:11 -0300

    Update combustivel.js

--- PtiAlgoritimo/combustivel.js
+++ PtiAlgoritimo/combustivel.js
@@ -1,7 +1,7 @@
 
 do{ 
-    var alc = parseInt(prompt('Digite o valor do Alcool'));
-    var gas = parseInt(prompt('Digite o valor da gasolina'));
+    var alc = parseFloat(prompt('Digite o valor do Alcool'));
+    var gas = parseFloat(prompt('Digite o valor da gasolina'));
     var calc = alc / gas;
     var res = calc;
             
@@ -28,8 +28,42 @@ do{
  }
 
    
+ 
+ // codigo Bruna
+ /*
+ do {
+    
+   var gas, alc, res, inicio
+   
+   gas = parseFloat(prompt("Digite o valor da gas"));
+ 
+   alc = parseFloat(prompt("Digite o valor do alc"));
 
-        
+   res = alc / gas;
+   
+     
+   
+     if (res > 0.7) {
+ 
+       alert('Compre gasolina');
+ 
+     } else if (res < 0.7) {
+ 
+      alert('compre álcool');
+ 
+   }else{
+
+       alert("Você pode escolher alcool ou gasolina");
+     }
+ 
+    inicio = prompt("Deseja novo calculo? \n Digite n para finalizar. Digite s para continuar");
+ 
+ }  while (inicio == 's'){
+
+   alert("Obrigado volte sempre!");
+ }
+
+        */
 
   
     
